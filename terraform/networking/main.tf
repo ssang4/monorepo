d53a030153dc6e82702aea51077189bb67cc014f
Shiuh Sen Ang <ssang@protonmail.com>
2022-07-11 19:12:17 +0200

    Added free tier account vpc

--- terraform/networking/main.tf
+++ terraform/networking/main.tf
@@ -5,5 +5,25 @@ module "vpc" {
   cidr = "10.0.0.0/16"
 
   azs = [ "eu-central-1a", "eu-central-1b", "eu-central-1c" ]
-  public_subnets = [ "10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24" ]
+  private_subnets = [ "10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24" ]
+  public_subnets = [ "10.0.10.0/24", "10.0.20.0/24", "10.0.30.0/24" ]
+
+  enable_nat_gateway = false
+}
+
+module "vpc-free-tier" {
+  source = "terraform-aws-modules/vpc/aws"
+  version = "3.14.2"
+
+  cidr = "172.16.0.0/16"
+
+  azs = [ "eu-central-1a", "eu-central-1b", "eu-central-1c" ]
+  private_subnets = [ "172.16.1.0/24", "172.16.2.0/24", "172.16.3.0/24" ]
+  public_subnets = [ "172.16.10.0/24", "172.16.20.0/24", "172.16.30.0/24" ]
+
+  enable_nat_gateway = false
+
+  providers = {
+    aws = aws.free-tier
+   }
 }
