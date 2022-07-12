a650eb9f3b0f3aa694e2a3aec7025d54994f8a51
Shiuh Sen Ang <ssang@protonmail.com>
2022-07-12 23:51:49 +0200

    Output cidr blocks from networking

--- terraform/networking/outputs.tf
+++ terraform/networking/outputs.tf
@@ -1,23 +1,31 @@
 output "vpc_id" {
-    value = module.vpc.vpc_id
+  value = module.vpc.vpc_id
 }
 
 output "private_subnets" {
-    value = module.vpc.private_subnets
+  value = module.vpc.private_subnets
 }
 
 output "public_subnets" {
-    value = module.vpc.public_subnets
+  value = module.vpc.public_subnets
+}
+
+output "vpc_cidr_block" {
+  value = module.vpc.vpc_cidr_block
 }
 
 output "vpc_id_free_tier" {
-    value = module.vpc-free-tier.vpc_id
+  value = module.vpc-free-tier.vpc_id
 }
 
 output "private_subnets_free_tier" {
-    value = module.vpc-free-tier.private_subnets
+  value = module.vpc-free-tier.private_subnets
 }
 
 output "public_subnets_free_tier" {
-    value = module.vpc-free-tier.public_subnets
+  value = module.vpc-free-tier.public_subnets
+}
+
+output "vpc_free_tier_cidr_block" {
+  value = module.vpc-free-tier.vpc_cidr_block
 }
