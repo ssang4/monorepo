5a24bce39bdc60f01c5835c7b434ccf366bf77b8
Shiuh Sen Ang <ssang@protonmail.com>
2022-09-20 22:29:37 +0200

    Migrate terraform state to s3

--- infrastructure/providers.tf
+++ infrastructure/providers.tf
@@ -1,11 +1,19 @@
 terraform {
+  backend "s3" {
+    bucket         = "ssang-terraform-state"
+    key            = "vault"
+    region         = "eu-central-1"
+    dynamodb_table = "ssang-terraform-lock"
+    profile        = "default"
+  }
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = "4.31.0"
     }
     digitalocean = {
-      source = "digitalocean/digitalocean"
+      source  = "digitalocean/digitalocean"
       version = "2.22.3"
     }
   }
--- vault/providers.tf
+++ vault/providers.tf
@@ -1,4 +1,12 @@
 terraform {
+  backend "s3" {
+    bucket         = "ssang-terraform-state"
+    key            = "infrastructure"
+    region         = "eu-central-1"
+    dynamodb_table = "ssang-terraform-lock"
+    profile        = "default"
+  }
+
   required_providers {
     vault = {
       source  = "hashicorp/vault"
@@ -9,5 +17,5 @@ terraform {
 
 provider "vault" {
   address = "https://vault.ssang.io"
-  token = var.vault_root_token
+  token   = var.vault_root_token
 }
